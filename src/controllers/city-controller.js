36f7f320e4340b8bfe1b570162407fe16a6345ca
Prachi Jain <96807804+prachi288@users.noreply.github.com>
2023-07-21 15:23:55 +0530

    Update city-controller.js

--- src/controllers/city-controller.js
+++ src/controllers/city-controller.js
@@ -25,6 +25,66 @@ async function createCity(req, res) {
     }
 }
 
+/**
+ * GET : /cities
+ * req-body {}
+ */
+async function getCities(req, res) {
+    try {
+        const cities= await CityService.getCities();
+        SuccessResponse.data = cities;
+        return res
+                .status(StatusCodes.OK)
+                .json(SuccessResponse);
+    } catch(error) {
+        ErrorResponse.error = error;
+        return res
+                .status(error.statusCode)
+                .json(ErrorResponse);
+    }
+}
+
+/**
+ * POST : /cities/:id 
+ * req-body {}
+ */
+async function getCity(req, res) {
+    try {
+        const city = await CityService.getCity(req.params.id);
+        SuccessResponse.data = city;
+        return res
+                .status(StatusCodes.OK)
+                .json(SuccessResponse);
+    } catch(error) {
+        ErrorResponse.error = error;
+        return res
+                .status(error.statusCode)
+                .json(ErrorResponse);
+    }
+}
+
+/**
+ * DELETE : /cities/:id
+ * req-body {}
+ */
+async function destroyCity(req, res) {
+    try {
+        const response = await CityService.destroyCity(req.params.id);
+        SuccessResponse.data = response;
+        return res
+                .status(StatusCodes.OK)
+                .json(SuccessResponse);
+    } catch(error) {
+        ErrorResponse.error = error;
+        return res
+                .status(error.statusCode)
+                .json(ErrorResponse);
+    }
+}
+
 module.exports = {
-    createCity
+    createCity,
+    getCities,
+    getCity,
+    destroyCity
 }
