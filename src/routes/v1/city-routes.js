9e893fb1adb782dfb274d59e2d3d1380206633be
Prachi Jain <96807804+prachi288@users.noreply.github.com>
2023-07-21 15:26:28 +0530

    Update city-routes.js

--- src/routes/v1/city-routes.js
+++ src/routes/v1/city-routes.js
@@ -10,5 +10,17 @@ router.post('/',
         CityMiddlewares.validateCreateRequest,
         CityController.createCity);
 
+// /api/v1/cities GET
+router.get('/', 
+        CityController.getCities);
+        
+// /api/v1/cities/:id GET
+router.get('/:id', 
+        CityController.getCity);
+        
+// /api/v1/cities/:id DELETE
+router.delete('/:id', 
+        CityController.destroyCity);
+
 
 module.exports = router;
