1dc50f9924a7c10327a62af1360a35630345d18d
Prachi Jain <96807804+prachi288@users.noreply.github.com>
2023-07-21 15:22:06 +0530

    Add files via upload

---
+++ city-services.js
@@ -0,0 +1,62 @@
+const {StatusCodes} = require('http-status-codes');
+
+const { CityRepository } = require('../repositories');
+const AppError = require('../utils/errors/app-error');
+
+const cityRepository = new CityRepository();
+
+async function createCity(data) {
+    try {
+        const city = await cityRepository.create(data);
+        return city;
+    } catch(error) {
+        if(error.name == 'SequelizeValidationError' || error.name == 'SequelizeUniqueConstraintError') {
+            let explanation = [];
+            error.errors.forEach((err) => {
+                explanation.push(err.message);
+            });
+            throw new AppError(explanation, StatusCodes.BAD_REQUEST);
+        }
+        throw new AppError('Cannot create a new city object', StatusCodes.INTERNAL_SERVER_ERROR);
+    }
+}
+
+async function getCities() {
+    try {
+        const cities = await cityRepository.getAll();
+        return cities;
+    } catch(error) {
+        throw new AppError('Cannot fetch data of all the cities', StatusCodes.INTERNAL_SERVER_ERROR);
+    }
+}
+
+async function getCity(id) {
+    try {
+        const city = await cityRepository.get(id);
+        return city;
+    } catch(error) {
+        if(error.statusCode == StatusCodes.NOT_FOUND) {
+            throw new AppError('The city you requested is not present', error.statusCode);
+        }
+        throw new AppError('Cannot fetch data of provided CityId', StatusCodes.INTERNAL_SERVER_ERROR);
+    }
+}
+
+async function destroyCity(id) {
+    try {
+        const response = await cityRepository.destroy(id);
+        return response;
+    } catch(error) {
+        if(error.statusCode == StatusCodes.NOT_FOUND) {
+            throw new AppError('The city you requested to delete is not present', error.statusCode);
+        }
+        throw new AppError('Cannot destroy the city', StatusCodes.INTERNAL_SERVER_ERROR);
+    }
+}
+
+module.exports = {
+    createCity,
+    getCities,
+    getCity,
+    destroyCity
+}
